e83f606d036d2c1d56c448cba743f9d160f23a36
Adam Pinter <adam.pinter@ymail.com>
2020-10-16 19:47:00 +0000

    hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,39 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+
 app = Flask(__name__)
+
+    # def pages
 @app.route('/')
-def hello_world():
-    return 'Hello, Napier!'
+def root():
+    return "Default, 'root' route "
+
+@app.route("/hello/")
+def hello():
+    return "Hello Napier!"
+
+@app.route("/goodbye/")
+def goodbye():
+    return "Goodbye cruel world :("
+
+@app.route("/private/")
+def private():
+    return redirect(url_for("login"))
+        
+    # redirect for private
+@app.route("/login")
+def login():
+    return "Login page"
+    
+    # custom 404
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Nothing to see here.", 404
+    
+    # force 404
+@app.route("/force404/")
+def force404():
+    abort(404)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+
