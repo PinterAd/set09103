a70ea3dd00f4303b642f03eaebc5d11d551fa763
PinterAd <59871478+PinterAd@users.noreply.github.com>
2020-12-01 19:09:53 +0000

    Update dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -1,16 +1,16 @@
 import sqlite3 as sql
 
 def insertUser(username,password):
-    con = sql.connect("database.db")
+    con = sql.connect("test.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (username,password))
+    cur.execute("INSERT INTO user (username,password) VALUES (?,?)", (username,password))
     con.commit()
     con.close()
 
 def retrieveUsers():
-	con = sql.connect("database.db")
+	con = sql.connect("test.db")
 	cur = con.cursor()
-	cur.execute("SELECT username, password FROM users")
+	cur.execute("SELECT username, password FROM user")
 	users = cur.fetchall()
 	con.close()
 	return users
