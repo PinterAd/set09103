5be226ea071ef0b54485070f3f6f583ee98cc1db
Adam Pinter <adam.pinter@ymail.com>
2020-12-02 12:05:09 +0000

    cleanup

--- testdb/testdb.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-with sqlite3.connect("test.db") as db:
-    cursor = db.cursor()
-cursor.execute('''
-        CREATE TABLE IF NOT EXISTS user(
-        username VARCHAR(20) NOT NULL,
-        password VARCHAR(20) NOT NULL);
-        ''')
-db.commit()
