47b1b733b75f5bed14157f37178aae2ef16c83c9
PinterAd <59871478+PinterAd@users.noreply.github.com>
2021-07-28 22:44:46 +0100

    Update note.py

--- note.py
+++ note.py
@@ -7,6 +7,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 
 app = Flask(__name__)
@@ -15,11 +16,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///var/database.db'
 Bootstrap(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app()
+login_manager.login_view = 'login'
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
@@ -64,7 +72,14 @@ def register():
         return '<h1> New user created</h1>'
     return render_template('register.html', form=form)
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
 @app.route('/upload')
+@login_required
 def upload():
     return render_template('upload.html')
 
