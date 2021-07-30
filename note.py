b73d0abd6d3b250844c7091265d0d9b045c6f62e
PinterAd <59871478+PinterAd@users.noreply.github.com>
2021-07-30 13:20:34 +0100

    Update note.py

--- note.py
+++ note.py
@@ -13,13 +13,13 @@ from flask_login import LoginManager, UserMixin, AnonymousUserMixin, login_user,
 def init ( app ) :
     config = configparser.configparser()
     try:
-        config_location ="etc/ defaults.cfg"
+        config_location ='etc/defaults.cfg'
         config.read( config_location )
-        app.config['DEBUG'] = config.get("config", "debug")
-        app.config['ip_address'] = config.get("config", "ip_address")
-        app.config['port'] = config.get("config", "port")
-        app.config['url'] = config.get("config ", "url")
-        app.config['SECRET_KEY'] = config.get("config", "secretkey")
+        app.config['DEBUG'] = config.get('config', 'debug')
+        app.config['ip_address'] = config.get('config', 'ip_address')
+        app.config['port'] = config.get('config', 'port')
+        app.config['url'] = config.get('config ', 'url')
+        app.config['SECRET_KEY'] = config.get('config', 'secretkey')
     except:print(" Could not read configs from : ", config_location )
 
 app = Flask(__name__)
